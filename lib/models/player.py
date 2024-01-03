4d79594056457988b8bc6a515b07f3c2b398fc4a
Zemorath <trentward10@gmail.com>
2024-01-02 19:05:17 -0600

    Getter and setter methods set for player class

--- lib/models/player.py
+++ lib/models/player.py
@@ -1,11 +1,13 @@
-
+from models.__init__ import CURSOR, CONN
 
 class Player:
 
-    all = []
+    all = {}
 
-    def __init__(self, name):
-        self._name = name
+    def __init__(self, name, player_class, id=None):
+        self.id = id
+        self.name = name
+        self.player_class = player_class
         Player.all.append(self)
 
     @property
@@ -14,7 +16,24 @@ class Player:
     
     @name.setter
     def name(self, name):
-        if (type(name) == str) and (1 <= len(name) <= 15):
+        if isinstance(name, str) and (1 <= len(name) <= 15):
             self._name = name
+        else:
+            raise ValueError(
+                "Name must be a non-empty string and no more than 15 characters"
+            )
+        
+    @property
+    def player_class(self):
+        return self._player_class
+    
+    @player_class.setter
+    def player_class(self, player_class):
+        if isinstance(player_class, str) and len(player_class):
+            self._player_class = player_class
+        else:
+            raise ValueError(
+                "player_class must be a non-empty string"
+            )
 
     
