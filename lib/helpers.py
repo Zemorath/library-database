2b0c81190c1fea505dbd6b8888015bfea5362208
Zemorath <trentward10@gmail.com>
2024-01-07 23:36:16 -0600

    First helpers added

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,4 +1,7 @@
 # lib/helpers.py
+from models.item import Item
+from models.player import Player
+from models.player_items import Player_Items
 
 def helper_1():
     print("Performing useful function#1.")
@@ -7,3 +10,8 @@ def helper_1():
 def exit_program():
     print("Goodbye!")
     exit()
+
+def list_items():
+    items = Item.get_all()
+    for item in items:
+        print(item)
