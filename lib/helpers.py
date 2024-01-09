82c083c47f32c1b564c98ba54bd08822c0f599da
Zemorath <trentward10@gmail.com>
2024-01-08 21:28:00 -0600

    Find by id and create helper methods added

--- lib/helpers.py
+++ lib/helpers.py
@@ -25,3 +25,21 @@ def find_item_by_name():
         f'Item {name} not found'
     ))
 
+def find_item_by_id():
+    id_ = input("Enter the item's id: ")
+    item = Item.find_by_id(id_)
+    print(item) if item else print(f'Item {id_} not found')
+
+def create_item():
+    name = input("Enter the item's name: ")
+    health = input("Enter the item's health: ")
+    defense = input("Enter the item's defense: ")
+    attack = input("Enter the item's attack: ")
+    crit_dmg = input("Enter the item's crit damage: ")
+    crit_chance = input("Enter the item's crit chance: ")
+    speed = input("Enter the item's speed: ")
+    try:
+        item = Item.create(name, health, defense, attack, crit_dmg, crit_chance, speed)
+        print(f'Success: {item}')
+    except Exception as exc:
+        print("Error creating item: ", exc)
