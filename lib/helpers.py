d844f3a6286a4f955fc6dd34a55867fd5573a59f
Zemorath <trentward10@gmail.com>
2024-01-08 20:57:48 -0600

    Find by name helper method added

--- lib/helpers.py
+++ lib/helpers.py
@@ -11,7 +11,17 @@ def exit_program():
     print("Goodbye!")
     exit()
 
+# Item model helper methods
+
 def list_items():
     items = Item.get_all()
     for item in items:
         print(item)
+
+def find_item_by_name():
+    name = input("Enter the item's name: ")
+    item = Item.find_by_name(name)
+    print(item if item else print(
+        f'Item {name} not found'
+    ))
+
