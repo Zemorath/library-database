0b7dd418417b498cc0d47967bfb9867d16adf3c9
Zemorath <trentward10@gmail.com>
2024-01-08 22:34:13 -0600

    CLI import and main method complete

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,23 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    list_items,
+    find_item_by_name,
+    find_item_by_id,
+    create_item,
+    update_item,
+    delete_item,
+    list_players,
+    find_player_by_name,
+    find_player_by_id,
+    create_player,
+    update_player,
+    delete_player,
+    list_player_items,
+    find_pi_by_id,
+    create_relationship,
+    update_relation,
+    delete_relation
 )
 
 
@@ -13,7 +29,39 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
+            list_items()
+        elif choice == "2":
+            find_item_by_name()
+        elif choice == "3":
+            find_item_by_id()
+        elif choice == "4":
+            create_item()
+        elif choice == "5":
+            update_item()
+        elif choice == "6":
+            delete_item()
+        elif choice == "7":
+            list_players()
+        elif choice == "8":
+            find_player_by_name()
+        elif choice == "9":
+            find_player_by_id()
+        elif choice == "10":
+            create_player()
+        elif choice == "11":
+            update_player()
+        elif choice == "12":
+            delete_player()
+        elif choice == "13":
+            list_player_items()
+        elif choice == "14":
+            find_pi_by_id()
+        elif choice == "15":
+            create_relationship()
+        elif choice == "16":
+            update_relation()
+        elif choice == "17":
+            delete_relation()
         else:
             print("Invalid choice")
 
