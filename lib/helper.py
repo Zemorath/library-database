6b4ca37ab9770355688bd21c929faedf25910c50
Zemorath <trentward10@gmail.com>
2024-01-26 23:34:23 -0600

    Find by title, author and isbn helper methods added

--- lib/helper.py
+++ lib/helper.py
@@ -12,3 +12,24 @@ def list_books():
     books = Book.get_all()
     for book in books:
         print(book)
+
+def find_book_by_title():
+    title = input("Enter the book's name: ")
+    book = Book.find_by_title(title)
+    print(book if book else print(
+        f'Book {title} not found'
+    ))
+
+def find_book_by_author():
+    author = input("Enter the book's author: ")
+    book = Book.find_by_author(author)
+    print(book if book else print(
+        f'Book {author} not found'
+    ))
+
+def find_book_by_isbn():
+    isbn = input("Enter the book's isbn: ")
+    book = Book.find_by_isbn(isbn)
+    print(book if book else print(
+        f'Book {isbn} not found'
+    ))
