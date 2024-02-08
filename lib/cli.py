0f3f1984baeaf8f31a47bf038b8ea971ddcc8ae6
Zemorath <trentward10@gmail.com>
2024-02-07 22:04:06 -0600

    Navigational bugs removed

--- lib/cli.py
+++ lib/cli.py
@@ -38,10 +38,11 @@ def main():
                 print("4: Back")
                 second_choice = int(input())
 
-                if second_choice == 1:
+                while second_choice == 1:
                     print("Here is a list of all owners! Choose one to see more information or edit")
                     owner_choice = 0
                     owners = Owner.get_all()
+                    print(len(owners))
                     while owner_choice != len(owners) + 2:
                         for i, owner in enumerate(owners, start=1):
                             print(f"{i}. {owner.name}")
@@ -52,11 +53,13 @@ def main():
                         owner_choice = int(input())
                         if owner_choice == len(owners) + 2:
                             print("Moving back")
-                        if owner_choice == len(owners) + 1:
+                            second_choice = 0
+                        elif owner_choice == len(owners) + 1:
                             print("Let's add a new owner!")
                             create_owner()
                             owner_choice = len(owners) + 2
-                        else:
+                            second_choice = 0
+                        elif owner_choice in range(1, len(owners) + 1):
                             book_choice = 0
                             chosen_owner = owners[owner_choice-1]
                             list = grab_owners_books(chosen_owner.id)
@@ -78,16 +81,17 @@ def main():
                                 book_choice = int(input())
                                 if book_choice == (len(list) + 4):
                                     print("Moving Back!")
-
                                 elif book_choice == (len(list) + 1):
                                     print(f"Adding new book to {chosen_owner.name}")
                                     create_book(chosen_owner.id)
                                     book_choice = len(list) + 4
+                                    owner_choice = 0
                                 
                                 elif book_choice == (len(list) + 2):
                                     print(f"Updating {chosen_owner.name}!")
                                     update_owner(chosen_owner.id)
                                     book_choice = len(list) + 4
+                                    owner_choice = 0
                                 
                                 elif book_choice == (len(list) + 3):
                                     print(f"Deleting {chosen_owner.name}")
@@ -95,8 +99,10 @@ def main():
                                     second_choice = 0
                                     owner_choice = len(owners) + 2
                                     book_choice = len(list) + 4
+                                    
+                                    
 
-                                else:
+                                elif book_choice in range(1, len(list) + 1):
                                     option_choice = 0
                                     while option_choice != 3:
                                         chosen_book = list[book_choice-1]
@@ -117,9 +123,8 @@ def main():
                                         elif option_choice == 2:
                                             print("Let's delete the book!")
                                             delete_book(chosen_book.id, chosen_book.title)
+                                            option_choice = 3
                                     book_choice = 0
-                            owner_choice = 0
-                    second_choice = 0
                 if second_choice == 2:
                     owner_choice = 0
                     owners = list_owners_by_age()
@@ -290,227 +295,6 @@ def main():
         else:
             print("Exiting program!")
             exit()
-
-    # first_choice = 0
-    # while first_choice != "x":
-    #     print("-Online Library-")
-    #     print("1: Interact with books")
-    #     print("2: Interact with owners")
-    #     print("3: Settings")
-    #     print("4: Quit")
-    #     first_choice = int(input())
-
-    #     if first_choice == 1:
-    #         second_choice = 0
-    #         while second_choice != 6:
-    #             print("--Books--")
-    #             print("1: List books")
-    #             print("2: Find a specific book")
-    #             print("3: Add a new book")
-    #             print("4: Update an existing book")
-    #             print("5: Delete a book")
-    #             print("6: Back")
-    #             second_choice = int(input())
-                
-    #             if second_choice == 1:
-    #                 third_choice = 0
-    #                 while third_choice != 3:
-    #                     print("---List Books---")
-    #                     print("1: All Books")
-    #                     print("2: By Author")
-    #                     print("3: Back")
-    #                     third_choice = int(input())
-
-    #                     if third_choice == 1:
-    #                         list_books()
-    #                     elif third_choice == 2:
-    #                         find_book_by_author()
-    #                     elif third_choice == 3:
-    #                         print("Moving Back!")
-    #                 second_choice = 0
-    #             elif second_choice == 2:
-    #                 third_choice = 0
-    #                 while third_choice != 4:
-    #                     print("---Find a Book---")
-    #                     print("1: By Title")
-    #                     print("2: By Author")
-    #                     print("3: By ISBN")
-    #                     print("4: Back")
-    #                     third_choice = int(input())
-
-    #                     if third_choice == 1:
-    #                         find_book_by_title()
-    #                     elif third_choice == 2:
-    #                         find_book_by_author()
-    #                     elif third_choice == 3:
-    #                         find_book_by_isbn()
-    #                     elif third_choice == 4:
-    #                         print("Moving Back!")
-    #                 second_choice = 0
-    #             elif second_choice == 3:
-    #                 print("---Creating New Book---")
-    #                 create_book()
-    #             elif second_choice == 4:
-    #                 print("---Updating Existing Book---")
-    #                 update_book()
-    #             elif second_choice == 5:
-    #                 print("---Deleting Book---")
-    #                 delete_book()
-    #             elif second_choice == 6:
-    #                 print("Moving Back!")
-    #         first_choice = 0
-    #     if first_choice == 2:
-    #         second_choice = 0
-    #         while second_choice != 6:
-    #             print("--Owners--")
-    #             print("1: List Owners")
-    #             print("2: Find a specific owner")
-    #             print("3: Add a new owner")
-    #             print("4: Update an existing owner")
-    #             print("5: Delete a owner")
-    #             print("6: Back")
-    #             second_choice = int(input())
-                
-    #             if second_choice == 1:
-    #                 third_choice = 0
-    #                 while third_choice != 4:
-    #                     print("---List Owners---")
-    #                     print("1: All Owners")
-    #                     print("2: By Age")
-    #                     print("3: By Favorite Genre")
-    #                     print("4: Back")
-    #                     third_choice = int(input())
-
-    #                     if third_choice == 1:
-    #                         print("Here is a list of all owners! Choose one to see more information or edit")
-    #                         owner_choice = 0
-    #                         owners = Owner.get_all()
-    #                         for i, owner in enumerate(owners, start=1):
-    #                             print(f"{i}. {owner.name}")
-    #                         while owner_choice != len(owners) + 1:
-    #                             owner_choice = int(input())
-    #                             if owner_choice == len(owners) + 1:
-    #                                 print("Moving back")
-    #                             else:
-    #                                 chosen_owner = owners[owner_choice-1]
-    #                                 print("OWNER")
-    #                                 print("---------")
-    #                                 print(f"{chosen_owner.name} || Age: {chosen_owner.age} || Favorite Genre: {chosen_owner.fav_genre}")
-    #                                 print(" ")
-    #                                 print("BOOKS: Select one to edit")
-    #                                 print("---------")
-    #                                 list = list_owners_books(chosen_owner.id)
-    #                                 print(" ")
-    #                                 print("Options")
-    #                                 print("---------")
-    #                                 print(f"{len(list) + 1}: Add a new book")
-    #                                 print(f"{len(list) + 2}: Back")
-    #                                 book_choice = 0
-    #                                 while book_choice != (len(list) + 2):
-    #                                     book_choice = int(input())
-    #                                     if book_choice == (len(list) + 2):
-    #                                         print("Moving Back!")
-    #                                     elif book_choice == (len(list) + 1):
-    #                                         print(f"Adding new book to {chosen_owner.name}")
-    #                                         create_book(chosen_owner.id)
-    #                                     else:
-    #                                         chosen_book = list[book_choice-1]
-    #                                         print(" ")
-    #                                         print("BOOK")
-    #                                         print("---------")
-    #                                         print(f"{chosen_book.title} || Author: {chosen_book.author} || ISBN: {chosen_book.isbn}")
-    #                                         print("---------")
-    #                                         print("1. Update Book")
-    #                                         print("2. Delete Book")
-    #                                         print("3. Back")
-    #                                         option_choice = 0
-    #                                         while option_choice != 3:
-    #                                             option_choice = int(input())
-    #                                             if option_choice == 1:
-    #                                                 print("Let's Update the Book!")
-    #                                                 update_book(chosen_book.id)
-    #                                             elif option_choice == 2:
-    #                                                 print("Let's delete the book!")
-    #                                                 delete_book(chosen_book.id, chosen_book.title)
-
-                                    
-
-
-    #                     elif third_choice == 2:
-    #                         print("Listing owners by age")
-    #                         list_owners_by_age()
-    #                     elif third_choice == 3:
-    #                         print("Listing owners by favorite genre")
-    #                         list_owners_by_fav_genre()
-    #                     elif third_choice == 4:
-    #                         print("Moving Back")
-    #                 second_choice = 0
-    #             elif second_choice == 2:
-    #                 print("---Finding Owner by Name---")
-    #                 find_owner_by_name()
-    #             elif second_choice == 3:
-    #                 print("---Creating New owner---")
-    #                 create_owner()
-    #             elif second_choice == 4:
-    #                 print("---Updating Existing owner---")
-    #                 update_owner()
-    #             elif second_choice == 5:
-    #                 print("---Deleting owner---")
-    #                 delete_owner()
-    #             elif second_choice == 6:
-    #                 print("Moving Back!")
-    #         first_choice = 0
-    #     if first_choice == 3:
-    #         second_choice = 0
-    #         while second_choice != 3:
-    #             print("--Settings--")
-    #             print("1: Modify Book Table")
-    #             print("2: Modify Owner Table")
-    #             print("3: Back")
-    #             second_choice = int(input())
-
-    #             if second_choice == 1:
-    #                 third_choice = 0
-    #                 while third_choice != 3:
-    #                     print("---Modifying Book Table---")
-    #                     print("1: Add Table")
-    #                     print("2: Drop Table")
-    #                     print("3: Back")
-    #                     third_choice = int(input())
-
-    #                     if third_choice == 1:
-    #                         print("Creating Book Table")
-    #                         Book.create_table()
-    #                     elif third_choice == 2:
-    #                         print("Dropping Book Table")
-    #                         Book.drop_table()
-    #                     elif third_choice == 3:
-    #                         print("Moving Back!")
-    #                 second_choice = 0
-    #             if second_choice == 2:
-    #                 owner_table_choice = 0
-    #                 while owner_table_choice != 3:
-    #                     print("---Modifying Owner Table---")
-    #                     print("1: Add Table")
-    #                     print("2: Drop Table")
-    #                     print("3: Back")
-    #                     owner_table_choice = int(input())
-
-    #                     if owner_table_choice == 1:
-    #                         print("Creating Owner Table")
-    #                         Owner.create_table()
-    #                     elif owner_table_choice == 2:
-    #                         print("Dropping Owner Table")
-    #                         Owner.drop_table()
-    #                     elif owner_table_choice == 3:
-    #                         print("Moving Back!")
-    #                 second_choice = 0
-    #             if second_choice == 3:
-    #                 print("Moving Back!")
-    #         first_choice = 0
-    #     if first_choice == 4:
-    #         print("Exiting Program!")
-    #         exit()
 
 if __name__ == "__main__":
     main()
